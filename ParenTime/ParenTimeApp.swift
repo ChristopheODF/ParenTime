413a8bf982228e2badebbe829abb1a7a41df421a
Christophe GAUDOUT <christophe.gaudout@contentsquare.com>
2026-01-22 09:10:59 +0100

    Initial Commit

---
+++ ParenTime/ParenTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParenTimeApp.swift
+//  ParenTime
+//
+//  Created by Christophe Gaudout on 22/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ParenTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParenTimeTests/ParenTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParenTimeTests.swift
+//  ParenTimeTests
+//
+//  Created by Christophe Gaudout on 22/01/2026.
+//
+
+import Testing
+@testable import ParenTime
+
+struct ParenTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
